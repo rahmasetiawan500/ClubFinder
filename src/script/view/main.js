2fe3a9232c6ee9a637209a416444d2c1b9c405c0
rahmasetiawan500 <rahmasetiawan500@gmail.com>
2020-09-16 00:20:17 +0700

    Costum Element : Membuat club-list dan club-item Component

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,9 +1,10 @@
+import '../component/club-list.js';
 import '../component/search-bar.js';
 import DataSource from '../data/data-source.js';
 
 const main = () => {
     const searchElement = document.querySelector("search-bar");
-    const clubListElement = document.querySelector("#clubList");
+    const clubListElement = document.querySelector("club-list");
     const onButtonSearchClicked = async () => {
         try{
             const result = await DataSource.searchClub(searchElement.value);
@@ -13,32 +14,14 @@ const main = () => {
         }
     };
 
-    const renderResult = results => {
-        clubListElement.innerHTML = "";
-        results.forEach(club => {
-            const {
-                name,
-                fanArt,
-                description
-            } = club;
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
-
-            clubElement.innerHTML = `
-                <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-                <div class="club-info">
-                    <h2>${name}</h2>
-                    <p>${description}</p>
-                </div>`;
-
-            clubListElement.appendChild(clubElement);
-        })
+	const renderResult = results => {
+        clubListElement.clubs = results;
     };
 
-    const fallbackResult = message => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+	const fallbackResult = message => {
+        clubListElement.renderError(message);
     };
+
 
     searchElement.clickEvent = onButtonSearchClicked;
 };
